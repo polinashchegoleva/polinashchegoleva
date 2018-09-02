c3cc00eb827e41614ff513a48fa84ab1eb934be6
polinashchegoleva <polina.cdm@gmail.com>
2018-09-02 14:05:20 -0700

    commit whith scroll jquery

---
+++ curs2poli/common.js
@@ -0,0 +1,10 @@
+//Плагин прокрутки
+
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+});
+
