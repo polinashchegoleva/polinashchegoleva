588b5a6ada3971430e128a01bb3858b7911f7cc7
polinashchegoleva <polina.cdm@gmail.com>
2018-09-14 10:27:08 -0700

    create project startup

---
+++ startup/common.js
@@ -0,0 +1,11 @@
+//плагин прокрутки к якорям
+
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+});
+
+
